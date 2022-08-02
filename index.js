71ca4723992a74863c778824d9308cf118f0e651
RodrigoFreitas-L <kdigo.chr@gmail.com>
2022-08-02 17:40:20 -0300

    Feat: criação de validações para campo de email e senha

--- index.js
+++ index.js
@@ -35,6 +35,16 @@ app.get('/talker/:id', async (req, res) => {
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
+  const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+  if(!email) return res.status(400).json({ message: "O campo \"email\" é obrigatório" });
+
+  if(!regex.test(email)) return res.status(400).json({ message: "O \"email\" deve ter o formato \"email@email.com\"" });
+
+  if(!password) return res.status(400).json({ message: "O campo \"password\" é obrigatório" });
+
+  if(password.length < 6) return res.status(400).json({ message: "O \"password\" deve ter pelo menos 6 caracteres"});
+
   res.status(200).json({ token: generateToken() });
 });
 
