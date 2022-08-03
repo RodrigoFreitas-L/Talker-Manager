be28e852c375dfb8bd442f6bc6bd6f9cf3ef439c
RodrigoFreitas-L <kdigo.chr@gmail.com>
2022-08-02 21:51:32 -0300

    ESLint corrigido

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const talker = require('./talker.json');
-const { readTalkers, generateToken, writeTalkers, modifyTalker } = require('./helpers');
+const fs = require('fs').promises;
+const { readTalkers, generateToken, writeTalkers } = require('./helpers');
 const { verifyName } = require('./middlewares/verifyName');
 const { verifyAge } = require('./middlewares/verifyAge');
 const { verifyTalk } = require('./middlewares/verifyTalk');
 const { verifyWatchedAt } = require('./middlewares/verifyWatchedAt');
 const { verifyRate } = require('./middlewares/verifyRate');
 const { verifyToken } = require('./middlewares/verifyToken');
-const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,7 +22,7 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (req, res) => {
   const getTalkers = await readTalkers();
-  if(!getTalkers) return res.status(200).json([]);
+  if (!getTalkers) return res.status(200).json([]);
   res.status(200).json(getTalkers);
 });
 
@@ -33,7 +32,7 @@ app.get('/talker/:id', async (req, res) => {
     const getTalkers = await readTalkers();
 
     const talkerById = getTalkers.find(({ id: talkerId }) => talkerId === Number(id));
-    if(!talkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    if (!talkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
     res.status(200).json(talkerById);
   } catch (error) {
     return res.status(500).end();
@@ -42,28 +41,50 @@ app.get('/talker/:id', async (req, res) => {
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
-  const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/;
 
-  if(!email) return res.status(400).json({ message: "O campo \"email\" é obrigatório" });
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
 
-  if(!regex.test(email)) return res.status(400).json({ message: "O \"email\" deve ter o formato \"email@email.com\"" });
+  if (!regex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
 
-  if(!password) return res.status(400).json({ message: "O campo \"password\" é obrigatório" });
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
 
-  if(password.length < 6) return res.status(400).json({ message: "O \"password\" deve ter pelo menos 6 caracteres"});
+  if (password.length < 6) {
+ return res.status(400)
+  .json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+}
 
   res.status(200).json({ token: generateToken() });
 });
 
-app.post('/talker', verifyToken, verifyName, verifyAge, verifyTalk, verifyWatchedAt, verifyRate, async (req, res) => {
+app.post(
+  '/talker',
+  verifyToken,
+  verifyName,
+  verifyAge,
+  verifyTalk, 
+  verifyWatchedAt,
+  verifyRate,
+  async (req, res) => {
   const { name, age, talk: { watchedAt, rate } } = req.body;
   const getTalkers = await readTalkers();
   const id = getTalkers.length + 1;
   await writeTalkers({ name, age, id, talk: { watchedAt, rate } });
   return res.status(201).json({ name, age, id, talk: { watchedAt, rate } });
-});
+},
+);
 
-app.put('/talker/:id',verifyToken, verifyName, verifyAge, verifyTalk, verifyWatchedAt, verifyRate, async (req, res) => {
+app.put(
+  '/talker/:id',
+  verifyToken,
+  verifyName,
+  verifyAge,
+  verifyTalk,
+  verifyWatchedAt,
+  verifyRate,
+  async (req, res) => {
   const { id } = req.params;
   const idToNumber = Number(id);
   const { name, age, talk: { watchedAt, rate } } = req.body;
@@ -73,7 +94,8 @@ app.put('/talker/:id',verifyToken, verifyName, verifyAge, verifyTalk, verifyWatc
   newTalker[talkerById] = { name, age, id: idToNumber, talk: { watchedAt, rate } };
   await fs.writeFile('talker.json', JSON.stringify(newTalker));
   return res.status(200).json({ name, age, id: idToNumber, talk: { watchedAt, rate } });
-});
+},
+);
 
 app.listen(PORT, () => {
   console.log('Online');
